ac1e761e250513dc1750138ef1f6eb1a81f84a8b
joalmay <joalmay@gmail.com>
2014-01-18 12:05:29 -0700

    Fixed Avatar
    Signed-off-by: joalmay <joalmay@gmail.com>

--- js/main.js
+++ js/main.js
@@ -32,8 +32,9 @@ $(function(){
     $('#tweet-submit').click(function() {
     	var name=$('#name').html();
     	var text= $('textarea').val();
+    	var avatar= "img/alagoon.jpg";
     	console.log(name +" -> "+ text);
-    	sendTweet(name,text);
+    	sendTweet(name, avatar, text);
     });
 
 });
@@ -64,17 +65,28 @@ function updateCountdown() {
 
 }
 
-function sendTweet(name, text){
+function sendTweet(name, avatar_src, text){
 	var tweet=document.createElement('div');
+	var profile_content=document.createElement('div');
 	var avatar=document.createElement('img');
+	var full_name=document.createElement('span');
 	var username=document.createElement('span');
 	var tweet_text=document.createElement('p');
 	var tweet_actions=document.createElement('div');
 
 	$(tweet).addClass("tweet");
-	$(username).addClass("username").text(name).appendTo(tweet);
+	$(profile_content).addClass("content");
+				
+	$(avatar).addClass("avatar").attr('src',avatar_src).appendTo(profile_content);
+	$(full_name).text(name).appendTo(profile_content);
+	$(username).addClass("username").text(name).appendTo(profile_content);
+
+	$(profile_content).appendTo(tweet);
+
 	$(tweet_text).addClass("tweet-text").text(text).appendTo(tweet);
 	$(tweet_actions).addClass("tweet-actions").appendTo(tweet);
+
+	//Send to the Stream
 	$('#stream').prepend(tweet);
 }
 
